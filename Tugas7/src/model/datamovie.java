227ab4167ae38c392dd9be5f22116a73de5bdb93
Faaris Sayyid Al-Ghozali <116880373+faaris23@users.noreply.github.com>
2024-05-18 20:18:49 +0700

    Add files via upload

---
+++ Tugas7/src/model/datamovie.java
@@ -0,0 +1,52 @@
+
+package model;
+
+public class datamovie {
+    private String judul;
+    private Double alur;
+    private Double penokohan;
+    private Double akting;
+    private Double nilai;
+
+    public String getJudul() {
+        return judul;
+    }
+
+    public void setJudul(String judul) {
+        this.judul = judul;
+    }
+    
+    public Double getAlur() {
+        return alur;
+    }
+
+    public void setAlur(Double alur) {
+        this.alur = alur;
+    }
+
+    public Double getPenokohan() {
+        return penokohan;
+    }
+
+    public void setPenokohan(Double penokohan) {
+        this.penokohan = penokohan;
+    }
+
+    public Double getAkting() {
+        return akting;
+    }
+
+    public void setAkting(Double akting) {
+        this.akting = akting;
+    }
+
+    public Double getNilai() {
+        return nilai;
+    }
+    
+    public void setNilai(Double nilai) {
+        this.nilai = nilai;
+    }
+    
+    
+}
